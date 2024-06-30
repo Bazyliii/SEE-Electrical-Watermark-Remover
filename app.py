1a6987a74ce2f18ca54461fa9d1c8065e70367dd
Jarosław Wierzbowski <jaroslawierzbowski2001@gmail.com>
2024-07-01 00:10:55 +0200

    Improved removal process soo It wont leave trash inside byte stream

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 from sys import argv
 
 if __name__ == "__main__":
@@ -7,7 +8,8 @@ Example: python .\app.py "pdf_file.pdf"''')
     file: str = argv[1]
     if file.endswith(".pdf"):
         with open(file, "rb") as f:
-            file_stream: bytes = f.read().replace(b"R\n/Image", b"")
+            file_stream: bytes = f.read()
+            file_stream = re.sub(rb"\n/Image.*", b"", file_stream)
         with open(file.strip(".pdf") + "_no_watermark.pdf", "wb") as f:
             f.write(file_stream)
     else:
